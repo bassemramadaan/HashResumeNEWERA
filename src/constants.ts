12a0d8040487230fd141c0ac2f130c3ba38aba57
Bassem <89530765+bassemramadaan@users.noreply.github.com>
2026-07-20 13:33:51 +0300

    feat: use new brand logo images from ibb.co CDN (icon, white, dark)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const LOGO_WHITE_URL = "/logos/logo-white.png";
-export const LOGO_BLACK_URL = "/logos/logo-dark.png";
-export const LOGO_ICON_URL = "/logos/logo-icon.png";
+export const LOGO_WHITE_URL = "https://i.ibb.co/tPN2Wtwd/IN-LOGO-icon-with-tag-3.png";
+export const LOGO_BLACK_URL = "https://i.ibb.co/p6bMBFQT/IN-LOGO-icon-with-tag-1.png";
+export const LOGO_ICON_URL = "https://i.ibb.co/qFFjyH8V/IN-LOGO-icon-3.png";
 export const LOGO_URL = LOGO_BLACK_URL;
 export const HERO_LOGO_URL = LOGO_BLACK_URL;
 
